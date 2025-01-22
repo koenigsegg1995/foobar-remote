5c982205a589edf157ef6a7fdff4a6e6535ca7a3
koenigsegg1995 <koenigsegg1995@gmail.com>
2025-01-22 23:58:48 +0800

    First commit

---
+++ remote-web/src/main/java/com/koeHo/foobarremote/FoobarRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.koeHo.foobarremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobarRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobarRemoteApplication.class, args);
+	}
+
+}
